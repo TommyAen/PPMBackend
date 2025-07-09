a9be884b334b15f23fa5428e6c39c9ec549daa3a
Tommaso Bellucci <tommyaen@gmail.com>
2025-07-09 13:21:16 +0200

    Additional change to method

--- accounts/views.py
+++ accounts/views.py
@@ -45,8 +45,13 @@ class ProfileDetailView(DetailView):
 def send_friend_request(request, user_id):
     to_user = get_object_or_404(CustomUser, id=user_id)
     if to_user != request.user:
-        FriendRequest.objects.get_or_create(from_user=request.user, to_user=to_user)
+        FriendRequest.objects.get_or_create(
+            from_user=request.user,
+            to_user=to_user,
+            defaults={'status': 'pending'}
+        )
     return HttpResponseRedirect(reverse('profile-detail', kwargs={'pk': to_user.id}))
+
 
 @login_required
 def accept_friend_request(request, request_id):
